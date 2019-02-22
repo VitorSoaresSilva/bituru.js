cefdd9547fc7e5120e2309aa27663884d1f1e776
Vitor Soares Silva <vitorcabeca2005@hotmail.com>
2019-02-22 12:11:59 -0300

    Some adjustments in the Clamp, and Abs functions

--- bituru.js
+++ bituru.js
@@ -1,10 +1,10 @@
-function clamp(max, min, x){
-    var dif1 = (max - x)
-	var dif2 = (x - min)
-    return x - (dif1 * (dif1>>31)) - (dif2 * (-dif2>>31))
+function clamp(min, max, x){
+    let dif1 = (max - x)
+	let dif2 = (x - min)
+    return x - (dif1 * (dif1>>31)) + (dif2 * (dif2>>31))
 }
 function abs(x) {
-    var t = x>>31
+    let t = x>>31
     return (x + (x*(t+t)))
 }
 function lerp(x,y,t){
